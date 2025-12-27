2b47207e5cb1148aa8880ea2c6d43653c37bd0ad
darshankumar17 <darshankumarthakare@gmail.com>
2025-12-27 17:54:43 +0530

    changed bucket name and dynamodb table name in backend.tf file to match the actual created resources.

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "darshan-app-project-terraform-state"
+    bucket = "terraform-automation-bucket-darshan"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-dynamoDB"
   }
 }
